94bb517423d6e8d3da0c3c3d3a89a2796db98c93
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2023-11-25 11:58:22 +0530

    Home hero div completed and added responsiveness

---
+++ src/Components/HomeHeroDiv.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './HomeHeroDiv.css'
+import { BsGithub } from 'react-icons/bs'
+import { MdEmail } from 'react-icons/md'
+import resume from "../assets/Pritam'sresume.pdf"
+import {
+    FaLinkedinIn,
+    FaCode
+} from 'react-icons/fa'
+
+const HomeHeroDiv = () => {
+    return (
+        <div className='HomeHeroDiv'>
+            <div className='HomeHeroDiv-innerContainer'>
+                <span className='small-tag'>Hi, I'm Pritam <FaCode className='facode' /></span>
+                <h2>A <span className='visbyfont'>Frontend Developer</span> with a passion of  coding,
+                    design and bringing ideas to
+                    life. <span className='oneliner'> One line of code at a time.</span></h2>
+
+                <div className='btn-container'>
+                    <a className='btn' href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+                    <Link className='round-btn github' to='https://github.com/anynomous001 '><BsGithub className='icon' /></Link>
+                    <Link className='round-btn github' to='https://www.linkedin.com/in/pritamchakroborty/'><FaLinkedinIn className='icon' /></Link>
+                    <Link className='round-btn github' to='https://mail.google.com/mail/mu/mp/354/#pr'><MdEmail className='icon' /></Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeHeroDiv
